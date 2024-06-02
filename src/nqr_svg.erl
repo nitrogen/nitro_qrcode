d47883ca08f6233be8ef0ad6aaf86329e833277d
Jesse Gumm <gumm@sigma-star.com>
2024-06-02 08:56:19 -0500

    Update build process a bit. Minor refactor of svg

--- src/nqr_svg.erl
+++ src/nqr_svg.erl
@@ -1,3 +1,18 @@
+% This module is Copyright 2024 Jesse Gumm <gumm@sigma-star.com>
+%
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+%
+% http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
+
+
 -module(nqr_svg).
 -export([tag/1]).
 
@@ -7,11 +22,12 @@ tag(Data) when is_list(Data) ->
     tag(list_to_binary(Data));
 tag(Data) when is_binary(Data) ->
     QrCode = nitro_qrcode:encode(Data),
-    Svg = pixels(QrCode),
     Dim = QrCode#qrcode.dimension,
+    Contents = iolist_to_binary(pixels(QrCode)),
+    Viewbox = iolist_to_binary(io_lib:format("0 0 ~p ~p", [Dim, Dim])),
     #{
-        viewbox=io_lib:format("0 0 ~p ~p", [Dim, Dim]),
-        data=Svg
+        viewbox=>Viewbox,
+        contents=>Contents
     }.
 
 pixels(#qrcode{data=Data, dimension=Dim}) ->
@@ -27,7 +43,7 @@ peel_row_bits(Data, Dim, RowNum) ->
     <<RowBits:Dim/bits, Bits/bits>> = Data,
     [{RowNum, RowBits} | peel_row_bits(Bits, Dim, RowNum+1)].
 
-process_rows(Rows) ->
+process_rows(Rows)  ->
     lists:map(fun({RowNum, Row}) ->
         process_bits(RowNum, 0, Row)
     end, Rows).
@@ -42,6 +58,3 @@ process_bits(RowNum, ColNum, <<0:1, Bits/bits>>) ->
 
 black_pixel(RowNum, ColNum) ->
     io_lib:format("<rect x='~p' y='~p' width='1' height='1' fill='black' />", [ColNum, RowNum]).
-
-dimension(QrCode) ->
-    QrCode#qrcode.dimension.
